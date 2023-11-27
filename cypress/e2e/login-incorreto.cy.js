988e42c65654a2eaf1252157231311fe4d5a5b6b
Henrique-Arau <penielh43@gmail.com>
2023-11-27 16:04:41 -0300

    cadastro e login incorreto

---
+++ cypress/e2e/login-incorreto.cy.js
@@ -0,0 +1,9 @@
+describe('Página de login', () => {
+    it('verificar mensagem de campos obrigatorios', () => {
+      cy.visit('http://localhost:4200/#/home')
+
+      cy.contains('User name is required!').should('be.visible');
+      cy.contains('Password is required!').should('be.visible');
+      
+    })
+  })
