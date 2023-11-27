23df65c3a4867411243ef80c5f24d5c49ffcac4b
Henrique-Arau <penielh43@gmail.com>
2023-11-27 15:14:10 -0300

    testes em registro de usuarios

--- cypress/e2e/testando.cy.js
+++ cypress/e2e/testando.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Página de cadastro', () => {
+  it('Preencher os campos do formulario corretamente para cadastrar um novo usuário', () => {
+    cy.visit('http://localhost:4200/#/home')
+    cy.contains('a', 'Register now').click();
+    cy.get('[data-test="email"]').type('teste@email.com.br');
+    cy.get('[data-test="fullName"]').type('Teste Alura Test Cypress');
+    cy.get('[data-test="registerUserName"]').type('testecy');
+    cy.get('[data-test="registerPassword"]').type('teste125');
+    cy.contains('button', 'Register').click();
   })
 })
