11221eee12254f6156c031af65af9402e9c0f701
Henrique-Arau <penielh43@gmail.com>
2023-11-27 17:52:25 -0300

    Personalizando os testes

--- cypress/e2e/login-correto.cy.js
+++ cypress/e2e/login-correto.cy.js
@@ -1,8 +1,13 @@
 describe('Página de cadastro', () => {
+
+  beforeEach(() => { 
+    cy.visit('http://localhost:4200/#/home')
+  })
+
     it('Preencher os campos do login corretamente para realizar login', () => {
-      cy.visit('http://localhost:4200/#/home')
       cy.get('[data-test="loginUserName"]').type('testecy');
       cy.get('[data-test="loginPassword"]').type('teste125');
       cy.contains('button', 'login').click();
+      
     })
   })
